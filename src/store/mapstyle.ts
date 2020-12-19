ca1506f0013170fbdb5003f20dd20288a71727bb
Jason Baker <jbaker@freightwaves.com>
2020-12-18 19:38:00 -0600

    Window.location instead of localhost

--- src/store/mapstyle.ts
+++ src/store/mapstyle.ts
@@ -6,8 +6,8 @@ const style: Style = {
   pitch: 60,
   bearing: 20,
   zoom: 12,
-  sprite: "http://localhost:8080/sprite",
-  glyphs: "http://localhost:8080/glyphs/{fontstack}/{range}.pbf",
+  sprite: `${window.location.href}/sprite`,
+  glyphs: `${window.location.href}/glyphs/{fontstack}/{range}.pbf`,
   sources: {
     carto: {
       type: "raster",
